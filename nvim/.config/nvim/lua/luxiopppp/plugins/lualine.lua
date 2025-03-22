bcc74e21eae043334a13a2ca81e0ca8020fb9cc4
Lucio Schraier <lucio.ese@gmail.com>
2025-03-22 13:13:09 -0300

    feat(nvim): better lualine

--- nvim/.config/nvim/lua/luxiopppp/plugins/lualine.lua
+++ nvim/.config/nvim/lua/luxiopppp/plugins/lualine.lua
@@ -1,27 +1,63 @@
+local function get_path()
+  local path = vim.fn.expand('%:p')
+  local icon, _ = require('nvim-web-devicons').get_icon(path)
+
+  local trimmed_path = vim.fn.fnamemodify(path, ":~3")
+
+  if icon then
+    return icon .. ' ' .. trimmed_path
+  else
+    return trimmed_path
+  end
+end
+
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
+  config = function ()
     local lualine = require("lualine")
     local lazy_status = require("lazy.status") -- to configure lazy pending updates count
+    local noice_status = require("noice.api.status")
+    local custom_gruvbox = require("lualine.themes.gruvbox-material")
 
-    -- configure lualine with modified theme
+    custom_gruvbox.visual.a.bg = "#98971a"
+    custom_gruvbox.insert.a.bg = "#458588"
+    custom_gruvbox.replace.a.bg = "#cc241d"
+
     lualine.setup({
       options = {
-        theme = "gruvbox-material",
+        theme = custom_gruvbox,
+        section_separators = '',
+        component_separators = '',
+        disabled_filetypes = { "NvimTree", }
       },
       sections = {
+        lualine_b = {
+          { "branch", icon = "", color = { gui = "bold" } },
+          { "diff" },
+          { "diagnostics" },
+        },
+        lualine_c = {
+          get_path
+        },
         lualine_x = {
+          { function () return vim.wo.spell and "Spell On" or "" end, color = { bg = "#b16286" } },
+          {
+            noice_status.command.get,
+            cond = noice_status.command.has,
+            color = { fg = "#fe8019" },
+          },
           {
             lazy_status.updates,
             cond = lazy_status.has_updates,
-            color = { fg = "#ff9e64" },
+            color = { fg = "#fe8019" },
           },
-          { "encoding" },
-          { "fileformat" },
-          { "filetype" },
         },
-      },
+        lualine_y = {},
+        lualine_z = {
+          { "location", padding = { left = 0, right = 1 } },
+        },
+      }
     })
   end,
 }
